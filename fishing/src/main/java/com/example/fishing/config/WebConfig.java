ae1102dc9193f6d7b945556712ab79b5a2efee29
ItzDommo <Justinleitol@gmail.com>
2025-11-21 20:21:38 +0100

    Git Pray

--- fishing/src/main/java/com/example/fishing/config/WebConfig.java
+++ fishing/src/main/java/com/example/fishing/config/WebConfig.java
@@ -1,26 +1,21 @@
 package com.example.fishing.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://webtech-fishing-projekt-frontend.onrender.com",
-                                "http://localhost:5173"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "https://play-click-n-fish.onrender.com",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
