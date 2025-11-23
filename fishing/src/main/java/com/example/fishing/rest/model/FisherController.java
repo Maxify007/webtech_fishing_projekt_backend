040d0e32b28b15bb42ff580f4df5ee9730c745a4
Snormax <maximinhduc@gmail.com>
2025-11-23 21:18:52 +0100

    PassiveFishing, Controller Anpassung mit Click, Passive(fisching) und Upgrade

--- fishing/src/main/java/com/example/fishing/rest/model/FisherController.java
+++ fishing/src/main/java/com/example/fishing/rest/model/FisherController.java
@@ -1,5 +1,6 @@
 package com.example.fishing.rest.model;
 
+import com.example.fishing.business.service.UpgradeType;
 import com.example.fishing.persistence.entity.Fisher;
 import com.example.fishing.persistence.entity.FisherRepository;
 import org.springframework.web.bind.annotation.*;
@@ -41,4 +42,33 @@ public class FisherController {
         }
         return fisher;
     }
+
+    @PostMapping("/{fisherId}/upgrade")
+    public Fisher upgrade(@PathVariable long fisherId,
+                          @RequestParam long playerId,
+                          @RequestParam UpgradeType type) {
+
+        Fisher fisher = getFisher(fisherId, playerId);
+        fisher.increaseLevelOf(type);
+        return fisherRepository.save(fisher);
+    }
+
+    @PostMapping("/{fisherId}/click")
+    public Fisher click(@PathVariable long fisherId,
+                        @RequestParam long playerId) {
+
+        Fisher fisher = getFisher(fisherId, playerId);
+        fisher.fishingAction();
+        return fisherRepository.save(fisher);
+    }
+
+    @PostMapping("/{fisherId}/passive")
+    public Fisher passive(@PathVariable long fisherId,
+                          @RequestParam long playerId) {
+
+        Fisher fisher = getFisher(fisherId, playerId);
+        fisher.passiveFishingTick(); // musst du evtl. noch hinzufügen
+        return fisherRepository.save(fisher);
+    }
+
 }
